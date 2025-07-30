d4fa080a057b448f945e3d1d8942096f89db61db
Sima Kumari <simak4484@gmail.com>
2025-07-31 00:41:44 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,80 @@
+//chapter1, operators & conditions
+// 77;
+// "Harry"
+// false
+//////////////////
+//Arithmatic Operator
+// console.log("Operators in Js")
+// let a = 50;
+// let b= 5;
+// console.log("a + b = ", a+b)
+// console.log("a - b = ", a-b)
+// console.log("a / b = ", a/b)
+// console.log("a * b = ", a*b)
+// console.log("a % b = ", a%b)
+// console.log("a ** b = ", a**b)
+// console.log("a = ",a)  //50
+// console.log("a++ = ", a++) //50
+// console.log("a = ", a)// 51
+// console.log("++a = ", ++a)//52
+// console.log("a-- = ", a--)//52
+// console.log("a = ", a)//51
+// console.log("--a = ", --a)//50
+
+///////////////////////////////
+//Assignment Operator
+// let a1 = 8; //a1 += 5 or a1 = a1+5 both are equal
+// a1 += 12
+// console.log(a1)
+// let a2 = 7;
+// a2 = a2+5
+// console.log(a2)
+// let b1 = 8; 
+// b1 -= 12
+// console.log(b1)
+// let c2 = 7;
+// c2 *= 5
+// console.log(c2)
+
+// let d1 = 8;
+// d1 /= 12
+// console.log(d1)
+// let e2 = 7;
+// e2 %= 5
+// console.log(e2)
+// let f1 = 8; 
+// f1 **= 12
+// console.log(f1)
+
+
+
+//////////////////
+// Comparison Operating
+let comp1 = 7;
+let comp2 = 6;
+console.log("comp1 == comp2 is ",comp1 == comp2)//equal to
+console.log("comp1 != comp2 is ",comp1 != comp2)//not equal
+console.log("comp1 === comp2 is ",comp1 === comp2)//equal  value and type
+console.log("comp1 !== comp2 is ",comp1 !== comp2)//not equal value or not equal type
+console.log("comp1 > comp2 is ",comp1 > comp2)//greater than
+
+////////////////////
+// Logical Operators
+let x = 5;
+let y = 8;
+console.log(x>y && x==5) 
+console.log(x<y && x==5)
+console.log(x>y || x==5) 
+console.log(x<y || x==5)
+console.log(!false)
+console.log(!true)
+
+
+
+
+
+
+
+
+
+
